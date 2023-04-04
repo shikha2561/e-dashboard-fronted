23e69032800a206522813fc614ad189dbfe8ea20
shikha <kannushikha15@gmail.com>
2023-04-04 12:33:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+       <div className="App">
+      <BrowserRouter >
+      <Nav />
+     <Routes>
+       
+       <Route path="/" element={<h1>Product Component</h1>}/>
+       <Route path="/add" element={<h1>AddProduct Componet</h1>}/>
+       <Route path="/update" element={<h1>Update Product Component </h1>}/>
+       <Route path="/logout" element={<h1> Logout Component</h1>} />
+       <Route path="/profile" element={<h1>Profile Component</h1>} />
+       
+
+       <Route path="/signup" element={<SignUp />} />
+      
+
+     </Routes>
+     </BrowserRouter>
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
+
